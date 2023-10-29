83ac5096fb06e0968ae748800e7c774cf81bcff7
Khushi-M-0521 <khushim0521@gmail.com>
2023-10-29 22:15:14 +0530

    dice_app_frame

--- dice/lib/main.dart
+++ dice/lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp());
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 28, 27, 27),
+              Color.fromARGB(255, 150, 147, 147),
+            ],
+            begin: Alignment.centerRight,
+            end: Alignment.centerLeft,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World!'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
-
