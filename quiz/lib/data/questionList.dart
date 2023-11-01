a3d301f1c7e915c27d915956cb0a97e1e6483db7
Khushi-M-0521 <khushim0521@gmail.com>
2023-11-01 23:11:50 +0530

    Added more questions

--- quiz/lib/data/questionList.dart
+++ quiz/lib/data/questionList.dart
@@ -55,4 +55,724 @@ const questions = [
       '.._ and _..',
     ],
   ),
+  QuizQuestion(
+    'Morse Code of MAN :',
+    [
+      '_ _   ._   _.',
+      '_ _   _.  ._',
+      '_.  _ _  ._',
+      '._.  _.  ._',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of MOON :',
+    [
+      '_ _   _ _ _   _ _ _   _.',
+      '_ _ _  _ _   _ _   _.',
+      '_.  _ _   _ _   _ _ _',
+      '_ _   _._   _._  _ _ _',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of SAD :',
+    [
+      '...  ._  _..',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of DARE :',
+    [
+      '_..  ._  ._. .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of SUIT :',
+    [
+      '...  .._ .. _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of MONSTER :',
+    [
+      '_ _   _ _ _  _.  ...  _  . ._.',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  ROOT:',
+    [
+      '._.  _ _ _   _ _ _   _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  : KIDS',
+    [
+      '_._  .. _..  ...',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  : ROME',
+    [
+      '._.  _ _ _  _ _  .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  : STRIKE',
+    [
+      '...  _  ._.  ..  _._  .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  : ATE',
+    [
+      '._  _  .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of EAT :',
+    [
+      '.  ._  _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  SORT:',
+    [
+      '...  _ _ _  ._.  _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  MOURN:',
+    [
+      '_ _   _ _ _   .._  ._.  _.',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of RATE :',
+    [
+      '._.  ._  _  .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  NOISE:',
+    [
+      '_.  _ _ _  ..  ...  .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  ARMER:',
+    [
+      '._  ._.  _ _  . ._.',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  KEEN:',
+    [
+      '_._  .  .  _.',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  STAR:',
+    [
+      '...  _  ._  ._.',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  DIET:',
+    [
+      '_..  ..  .  _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of REST :',
+    [
+      '._.  .  ...  _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of STORM :',
+    [
+      '...  _  _ _ _  ._.  _ _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of KNOT :',
+    [
+      '_._  _.  _ _ _   _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of NOSE :',
+    [
+      '_.  _ _ _  ...  .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of ROSE :',
+    [
+      '._.  _ _ _   ...  .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of STROKE :',
+    [
+      '...  _  ._.  _ _ _   _._ .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of SNORE :',
+    [
+      '...  _.  _ _ _  .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of TEST :',
+    [
+      '_  .  ...  _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  MAKE:',
+    [
+      '_ _   ._   _._   .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  MONKS:',
+    [
+      '_ _   _ _ _   _.   _._   ...',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  STUD:',
+    [
+      '...   _   .._   _..',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  EDIT:',
+    [
+      '.   _..   ..   _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  ROUTINE:',
+    [
+      '._.   _ _ _   .._   _   ..   _.   .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  DEMONSTRATION:',
+    [
+      '_..   .   _ _   _ _ _   _.   ...   _   ._.   ._   _   ..   _ _ _   _.',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  NITRATE:',
+    [
+      '_.   ..   _   ._.   ._   _   .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  NEURON:',
+    [
+      '_.   .   .._   ._.   _ _ _   _.',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  STADIUM:',
+    [
+      '...   _   ._   _..   ..   .._   _ _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  STUDIOUS:',
+    [
+      '...   _   .._   _..   ..   _ _ _   .._   ...',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  STUDENT:',
+    [
+      '...   _   .._   _..   .   _.   _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  STATUS:',
+    [
+      '...   _   ._   _   .._   ...',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  NONSENSE:',
+    [
+      '_.   _ _ _   _.   ...   .   _.   ...   .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  SMART:',
+    [
+      '...   _ _   ._   ._.   _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  IDIOT:',
+    [
+      '..   _..   ..   _ _ _   _',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  KITE:',
+    [
+      '_._   ..   _   .',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Morse Code of  KINDER:',
+    [
+      '_._   ..   _.   _..   .   ._.',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _ _   _ _ _   _ _   .   _.   _   .._   _ _   :',
+    [
+      'MOMENTUM',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _ _   _ _ _   .._   ...   .   :',
+    [
+      'MOUSE',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _ _ _   ._.   .   :',
+    [
+      'ORE',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _ _ _   _..   _ _ _   .._   ._.   :',
+    [
+      'ODOUR',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _..   .   ._   _ _   _ _ _   _.   :',
+    [
+      'DEAMON',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  .._   ...   .   ...   :',
+    [
+      'USES',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _   _ _ _   .._   ._.   ..   ...   _   :',
+    [
+      'TOURIST',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  ._.   ._   _..   ..   .._   ...   :',
+    [
+      'RADIUS',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _   ._   _ _   .   :',
+    [
+      'TAME',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  ._.   ..   ...   _._   :',
+    [
+      'RISK',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _ _   .   _   .   ._.   _ _ _   ..   _..   :',
+    [
+      'METEROID',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _ _   .   _   ._.   _ _ _   :',
+    [
+      'METRO',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  .._   ._.   ..   _.   .   :',
+    [
+      'URINE',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _..   ..   ._   _ _   _ _ _   _.   _..   :',
+    [
+      'DIAMOND',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _ _   ._   ._.   _._   ...   :',
+    [
+      'MARKS',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  ...   _ _   _ _ _   _._   .   :',
+    [
+      'SMOKE',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _..   ._.   ..   _.   _._   :',
+    [
+      'DRINK',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _..   ..   ...   _   ._.   .   ...   ...   :',
+    [
+      'DISTRESS',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _..   .   _.   _ _   ._   ._.   _._   :',
+    [
+      'DENMARK',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _ _   _ _ _   ._.   _   ._   ._.   :',
+    [
+      'MORTAR',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  ..   _.   _..   ..   ._   :',
+    [
+      'INDIA',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  ._   ...   ..   ._   :',
+    [
+      'ASIA',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  ..   _.   _..   _ _ _   _.   .   ._   ...   ..   ._   :',
+    [
+      'INDONEASIA',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  ._.   .._   ...   ...   ..   ._   :',
+    [
+      'RUSSIA',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  .._   _._   ._.   ._   ..   _.   .   :',
+    [
+      'UKRAINE',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  .   ...   _._   ..   _ _   _ _ _   :',
+    [
+      'ESKIMO',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _._   ._   ._.   _.   ._   _   ._   _._   ._   :',
+    [
+      'KARNATAKA',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  .   ._   ...   _   .   ._.   _.   :',
+    [
+      'EASTERN',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  ...   _   ._   _   .   _ _   .   _.   _   :',
+    [
+      'STATEMENT',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  ...   _._   ._   _   .   :',
+    [
+      'SKATE',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _ _   ..   _ _   .   :',
+    [
+      'MIME',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _ _   .   _..   ..   _   ._   _   ..   _ _ _   _.   :',
+    [
+      'MEDITATION',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  _   ._.   .._   ...   _   :',
+    [
+      'TRUST',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  ..   _.   ...   _   ._   _.   _   :',
+    [
+      'INSTANT',
+      '',
+      '',
+      '',
+    ],
+  ),
+  QuizQuestion(
+    'Decode Morse Code of  ..   _.   ...   ._   _.   .   :',
+    [
+      'INSANE',
+      '',
+      '',
+      '',
+    ],
+  ),
 ];
