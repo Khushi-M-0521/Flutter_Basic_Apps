62f93b80f802677c6e8e440fd65b75c99c5f42c3
Khushi-M-0521 <khushim0521@gmail.com>
2023-11-07 12:08:10 +0530

    reduced the size of summary list

--- quiz/lib/summary.dart
+++ quiz/lib/summary.dart
@@ -10,7 +10,7 @@ class ResSummary extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 500,
+      height: 450,
       child: SingleChildScrollView(
         child: Column(
           children:
