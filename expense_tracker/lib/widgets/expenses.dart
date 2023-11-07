400ec1f4b9b0977980481518e1d328e49de67917
Khushi-M-0521 <khushim0521@gmail.com>
2023-11-07 20:58:13 +0530

    Add or remove an expense

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -12,9 +12,6 @@ class Expenses extends StatefulWidget {
   }
 }
 
-
-
-
 class _ExpensesState extends State<Expenses> {
   final List<Expense> _regiesteredExpenses = [
     Expense(
@@ -31,12 +28,53 @@ class _ExpensesState extends State<Expenses> {
     )
   ];
 
-  void _openAddExpenseOverlay(){
-    showModalBottomSheet(context: context, builder: (ctx)=> const NewExpense());
+  void addNewExpense(Expense exp) {
+    setState(() {
+      _regiesteredExpenses.add(exp);
+    });
+  }
+
+  void _removeExpense(Expense exp) {
+    setState(() {
+      _regiesteredExpenses.remove(exp);
+    });
+  }
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => NewExpense(addNewExpense),
+      useSafeArea: true,
+      isScrollControlled: true,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget Content = Center(
+      child: Column(
+        children: [
+          Opacity(
+            opacity: 0.6,
+            child: Image.asset(
+              'assests/images/noExpenses.png',
+              // scale: 300,
+            ),
+          ),
+          const Text(
+            'No Expenses found',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 97, 96, 96)
+            ),
+          ),
+          const Text('Start tracking your Expenses, NOW !,',
+          style: TextStyle(fontSize: 20),),
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -50,7 +88,13 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text('the chart'),
-          Expanded(child: ExpensesList(expenses: _regiesteredExpenses)),
+          (_regiesteredExpenses.isEmpty)
+              ? Content
+              : Expanded(
+                  child: ExpensesList(
+                  expenses: _regiesteredExpenses,
+                  onRemovedExpense: _removeExpense,
+                )),
         ],
       ),
     );
--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,46 +1,162 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
-class NewExpense extends StatefulWidget{
-  const NewExpense({super.key});
+// ignore: must_be_immutable
+class NewExpense extends StatefulWidget {
+  NewExpense(this.addNewExpense, {super.key});
+
+  void Function(Expense) addNewExpense;
 
   @override
   State<NewExpense> createState() {
     return _NewExpense();
   }
-
 }
 
-
-
-class _NewExpense extends State<NewExpense>{
+class _NewExpense extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amtController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.work;
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amtController.dispose();
     super.dispose();
+  }
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  void _submitExpense() {
+    final amt = double.tryParse(_amtController.text);
+    final isValidAmt = amt == null || amt <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        isValidAmt ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid Expense'),
+          content: const Text(
+              'Please enter a valid title, amount, data and category'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text('Okay'),
+            )
+          ],
+        ),
+      );
+      return;
+    }
+    final newExp = Expense(
+      title: _titleController.text,
+      amount: amt,
+      date: _selectedDate!,
+      category: _selectedCategory,
+    );
+    widget.addNewExpense(newExp);
+    Navigator.pop(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    return   Padding(
-      padding:const  EdgeInsets.all(16),
-      child: Column(children: [
-        TextField(
-          controller: _titleController,
-          keyboardType: TextInputType.text,
-          maxLength: 50,
-          decoration: const InputDecoration(label: Text('Title')),
-        ),
-        Row(
-          children: [
-            ElevatedButton(onPressed: (){
-              print(_titleController.text);
-            }, child: const Text('Save Expense'))
-          ],
-        )
-      ],),
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            keyboardType: TextInputType.text,
+            maxLength: 50,
+            decoration: const InputDecoration(label: Text('Title')),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amtController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    label: Text('Amount'),
+                    prefixText: '₹ ',
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? 'No Selected Date'
+                          : formatter.format(_selectedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(
+                        Icons.calendar_month,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 15),
+          Row(
+            children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name.toUpperCase(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              const Spacer(),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                  onPressed: _submitExpense, child: const Text('Save Expense'))
+            ],
+          )
+        ],
+      ),
     );
   }
-
 }
