9c321aa9f76b25ebb4f84ca67a23149601a19cc8
Khushi-M-0521 <khushim0521@gmail.com>
2023-11-14 19:45:08 +0530

    few chart features

--- expense_tracker/lib/widgets/chart/chart.dart
+++ expense_tracker/lib/widgets/chart/chart.dart
@@ -2,7 +2,7 @@ import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/chart/chart_bar.dart';
 import 'package:flutter/material.dart';
 
-enum ChartType {all, lastMonth, specificDate}
+enum ChartType { all, lastMonth, specificDate }
 
 class Chart extends StatefulWidget {
   const Chart(this._allExpenses, {super.key});
@@ -16,25 +16,25 @@ class Chart extends StatefulWidget {
 }
 
 class _ChartState extends State<Chart> {
-
-  ChartType _chartType=ChartType.all;
-  List<Expense> _chartExpenses=[];
+  ChartType _chartType = ChartType.all;
+  List<Expense> _chartExpenses = [];
   DateTime? _specificDate;
   IconButton? _dateIconPiceker;
 
   @override
-  void initState(){
-    _chartExpenses=widget._allExpenses;
-    _dateIconPiceker=IconButton(onPressed: _datePicker, icon: const Icon(Icons.calendar_month));
+  void initState() {
+    _chartExpenses = widget._allExpenses;
+    _dateIconPiceker = IconButton(
+        onPressed: _datePicker, icon: const Icon(Icons.calendar_month));
     super.initState();
   }
 
-  void _datePicker()async{
+  void _datePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
     final pickedDate = await showDatePicker(
       context: context,
-      initialDate: _specificDate==null?now:_specificDate!,
+      initialDate: _specificDate == null ? now : _specificDate!,
       firstDate: firstDate,
       lastDate: now,
     );
@@ -44,57 +44,65 @@ class _ChartState extends State<Chart> {
     });
   }
 
-  void _changeChartType(ChartType? value){
-    if(value==null)
-    {
+  void _changeChartType(ChartType? value) {
+    if (value == null) {
       return;
     }
     setState(() {
       _chartType = value;
-      if(_chartType==ChartType.all){
-        _chartExpenses=widget._allExpenses;
-      }
-      else if(_chartType==ChartType.lastMonth) {
-        final now=DateTime.now();
-        final lastMonthDate=DateTime(now.year,now.month-1,now.day);
-        _chartExpenses=[];
-        for(final exp in widget._allExpenses){
-          if(exp.date.isAfter(lastMonthDate)){
+      if (_chartType == ChartType.all) {
+        _chartExpenses = widget._allExpenses;
+      } else if (_chartType == ChartType.lastMonth) {
+        final now = DateTime.now();
+        final lastMonthDate = DateTime(now.year, now.month - 1, now.day);
+        _chartExpenses = [];
+        for (final exp in widget._allExpenses) {
+          if (exp.date.isAfter(lastMonthDate)) {
             _chartExpenses.add(exp);
+          }
         }
-        }
-      }
-      else if(_chartType==ChartType.specificDate){
-        _chartExpenses=[];
-        for(final exp in widget._allExpenses){
-          if(exp.date.isAfter(_specificDate!)){
+      } else if (_chartType == ChartType.specificDate) {
+        _chartExpenses = [];
+        for (final exp in widget._allExpenses) {
+          if (exp.date.isAfter(_specificDate!)) {
             _chartExpenses.add(exp);
-        }
+          }
         }
       }
     });
   }
 
-  List<ExpenseBucket> get buckets{
-    return Category.values.map((category) => ExpenseBucket.forCategory(_chartExpenses,category)).toList();
+  List<ExpenseBucket> get buckets {
+    return Category.values
+        .map((category) => ExpenseBucket.forCategory(_chartExpenses, category))
+        .toList();
   }
 
-  double get maxTotalExpense{
-    double maxTotalExpense=0;
-    for(final bucket in buckets){
-      if(bucket.totalExpenses>maxTotalExpense){
-        maxTotalExpense=bucket.totalExpenses;
+  double get maxTotalExpense {
+    double maxTotalExpense = 0;
+    for (final bucket in buckets) {
+      if (bucket.totalExpenses > maxTotalExpense) {
+        maxTotalExpense = bucket.totalExpenses;
       }
     }
     return maxTotalExpense;
   }
 
+  double get totalExpense {
+    double amt = 0;
+    for (final exp in _chartExpenses) {
+      amt += exp.amount;
+    }
+    return amt;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(20),
+      margin: const EdgeInsets.all(10),
       padding: const EdgeInsets.symmetric(
-        horizontal: 15,
+        horizontal: 10,
+        vertical: 0,
       ),
       width: double.infinity,
       height: 200,
@@ -113,50 +121,108 @@ class _ChartState extends State<Chart> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-        DropdownButton(
-        value: _chartType ,
-        items: [
-          DropdownMenuItem(
-            value: ChartType.all,
-            child: Text('All Expenses',style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.normal),
-            ),),
-          DropdownMenuItem(
-            value: ChartType.lastMonth,
-            child: Text('Last 30 Days',style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.normal),
-            ),),
-          DropdownMenuItem(
-            value: ChartType.specificDate,
+          SizedBox(
+            height: 50,
             child: Row(
               children: [
-                Text('Specific Date: ${_specificDate==null? '' : formatter.format(_specificDate!)}',
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.normal),
-                ),
-                _dateIconPiceker!,
+                DropdownButton(
+                    value: _chartType,
+                    items: [
+                      DropdownMenuItem(
+                        value: ChartType.all,
+                        child: Text(
+                          'All Expenses',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .copyWith(fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                      DropdownMenuItem(
+                        value: ChartType.lastMonth,
+                        child: Text(
+                          'Last 30 Days',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .copyWith(fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                      DropdownMenuItem(
+                        value: ChartType.specificDate,
+                        child: Row(
+                          children: [
+                            Text(
+                              'Specific Date: ${_specificDate == null ? '' : formatter.format(_specificDate!)}',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleLarge!
+                                  .copyWith(fontWeight: FontWeight.normal),
+                            ),
+                            _dateIconPiceker!,
+                          ],
+                        ),
+                      ),
+                    ],
+                    onChanged: _changeChartType),
+                const Spacer(),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Total',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge!
+                          .copyWith(fontWeight: FontWeight.normal, fontSize: 12,),
+                    ),
+                    Text(
+                      '₹ ${totalExpense.toStringAsFixed(2)}',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge!
+                          .copyWith(fontWeight: FontWeight.normal, fontSize: 14,decoration: TextDecoration.overline),
+                    ),
+                  ],
+                )
               ],
-            ),),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                for (final bucket in buckets)
+                  Chart_Bar(
+                    amt: bucket.totalExpenses,
+                    fill: bucket.totalExpenses == 0
+                        ? 0
+                        : bucket.totalExpenses / maxTotalExpense,
+                  )
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: buckets
+                .map(
+                  (bucket) => Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: Icon(
+                        categoryIcons[bucket.category],
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                  ),
+                )
+                .toList(),
+          )
         ],
-        onChanged: _changeChartType),
-        const SizedBox(height: 15,),
-        Expanded(child: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            for(final bucket in buckets)
-              Chart_Bar(
-                fill: bucket.totalExpenses==0?0:bucket.totalExpenses/maxTotalExpense,
-              )
-          ],
-        ),),
-        const SizedBox(height: 10),
-        Row(
-          children: buckets.map((bucket) => Expanded(
-            child: Padding(padding: const EdgeInsets.all(4),
-            child: Icon(
-              categoryIcons[bucket.category],
-              color: Theme.of(context).colorScheme.secondary,
-            ),),
-          ),).toList(),
-        )
-      ],),
+      ),
     );
   }
 }
--- expense_tracker/lib/widgets/chart/chart_bar.dart
+++ expense_tracker/lib/widgets/chart/chart_bar.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
-class Chart_Bar extends StatelessWidget{
-  const Chart_Bar({super.key,required this.fill});
+class Chart_Bar extends StatelessWidget {
+  const Chart_Bar({super.key, required this.fill, required this.amt});
 
   final double fill;
+  final double amt;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Padding(padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 7),
       child: FractionallySizedBox(
-        heightFactor: fill,
+        heightFactor: fill < 0.25 ? 0.25 : fill,
         child: DecoratedBox(
           decoration: BoxDecoration(
             shape: BoxShape.rectangle,
             borderRadius: const BorderRadius.vertical(top: Radius.circular(5)),
             color: Theme.of(context).colorScheme.secondary,
           ),
+          child: Center(
+            child: Text(
+              '₹ ${amt.toStringAsFixed(2)}',
+              style:
+                  TextStyle(color: Theme.of(context).colorScheme.onSecondary),
+            ),
+          ),
         ),
-      ),) 
-      );
+      ),
+    ));
   }
-
 }
